cad3f967d5093321463f389b066486a14171d116
Sheena O'Connell <sheena.oconnell@gmail.com>
2020-12-22 09:18:25 +0200

    alive

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ GAE_PROJECT="umuzi-prod"
 VERSION=$(git rev-parse --short HEAD)$(date "+%F-%H%M") 
 
 
-gcloud app deploy app-staging.yaml --project $GAE_PROJECT --version "$VERSION" --no-promote
+gcloud app deploy app.yaml --project $GAE_PROJECT --version "$VERSION" --no-promote
